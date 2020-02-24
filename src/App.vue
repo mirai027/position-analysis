6a06dcc0a565c2478faadc38195ee0750f0f5b52
constWHITE <1163302785@qq.com>
2020-02-24 23:12:18 +0800

    :tada: init

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<template>
+  <div id="app">
+    <router-view />
+  </div>
+</template>
+
+<style lang="scss">
+@import '~@/styles/reset.scss';
+</style>
---
+++ src/views/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>init</div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
