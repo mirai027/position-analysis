d71216f68ebdbb04330b0498b6d2559974050bb5
lucifer021 <1163302785@qq.com>
2020-03-21 14:50:11 +0800

    :heavy_plus_sign: Add ElementUI

--- src/main.js
+++ src/main.js
@@ -3,9 +3,12 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import echarts from 'echarts'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 
 Vue.prototype.$echarts = echarts
 Vue.config.productionTip = false
+Vue.use(ElementUI)
 
 new Vue({
   router,
