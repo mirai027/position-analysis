0d88b408ce88858e00af163f79e6193e1fa9d052
lucifer021 <1163302785@qq.com>
2020-04-04 21:32:14 +0800

    🕯 深切悼念新冠肺炎疫情牺牲烈士和逝世同胞！

--- src/App.vue
+++ src/App.vue
@@ -37,6 +37,14 @@ export default {
   height: 100vh;
   display: flex;
   overflow: hidden;
+  filter: grayscale(100%);
+  -webkit-filter: grayscale(100%);
+  -moz-filter: grayscale(100%);
+  -ms-filter: grayscale(100%);
+  -o-filter: grayscale(100%);
+  filter: url("data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg'><filter id='grayscale'><feColorMatrix type='matrix' values='0.3333 0.3333 0.3333 0 0 0.3333 0.3333 0.3333 0 0 0.3333 0.3333 0.3333 0 0 0 0 0 1 0'/></filter></svg>#grayscale");
+  filter: progid:DXImageTransform.Microsoft.BasicImage(grayscale=1);
+  -webkit-filter: grayscale(1);
   .el-menu-vertical-demo {
     height: 100%;
     overflow: auto;
