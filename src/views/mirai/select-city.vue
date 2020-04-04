ed22b87ca5cf04799af9f4b7d499357754df2810
lucifer021 <1163302785@qq.com>
2020-04-04 21:22:24 +0800

    :sparkles: 完成二级城市级联选择

--- src/views/mirai/select-city.vue
+++ src/views/mirai/select-city.vue
@@ -1,65 +1,68 @@
 <template>
-  <el-select v-model="value" placeholder="请选择">
-    <el-option-group
-      v-for="group in cityOptions"
-      :key="group.label"
-      :label="group.label"
-    >
-      <el-option
-        v-for="item in group.options"
-        :key="item.value"
-        :label="item.label"
-        :value="item.value"
-      >
-      </el-option>
-    </el-option-group>
-  </el-select>
+  <div>
+    <el-cascader
+      ref="elcascader"
+      v-model="value"
+      :options="options"
+      :show-all-levels="false"
+      filterable
+      :props="{ expandTrigger: 'hover', checkStrictly: true }"
+      @change="changeVal"
+    ></el-cascader>
+  </div>
 </template>
 
 <script>
+import options from './country-date'
 export default {
   data() {
     return {
-      cityOptions: [
-        {
-          label: '热门城市',
-          options: [
-            {
-              value: 'Shanghai',
-              label: '上海'
-            },
-            {
-              value: 'Beijing',
-              label: '北京'
-            }
-          ]
-        },
-        {
-          label: '城市名',
-          options: [
-            {
-              value: 'Chengdu',
-              label: '成都'
-            },
-            {
-              value: 'Shenzhen',
-              label: '深圳'
-            },
-            {
-              value: 'Guangzhou',
-              label: '广州'
-            },
-            {
-              value: 'Dalian',
-              label: '大连'
-            }
-          ]
-        }
-      ],
-      value: ''
+      options: options,
+      value: ['中国', '北京市']
+    }
+  },
+  mounted() {},
+  methods: {
+    changeVal() {
+      this.$refs.elcascader.dropDownVisible = false
     }
   }
 }
 </script>
 
-<style></style>
+<style lang="scss">
+.el-cascader-menu {
+  &:first-child {
+    min-width: 120px;
+  }
+  &:nth-child(2) {
+    min-width: 240px;
+  }
+  // min-height: 300px;
+}
+.el-cascader-menu__wrap {
+  height: 304px;
+}
+.el-scrollbar__wrap {
+  margin-right: -10px !important;
+}
+.el-scrollbar__bar.is-vertical > div {
+  width: 50%;
+}
+.el-scrollbar__bar {
+  right: -2px;
+}
+
+.el-cascader-panel .el-radio {
+  width: 100%;
+  height: 100%;
+  z-index: 10;
+  position: absolute;
+  top: 0px;
+  right: 0px;
+}
+
+.el-cascader-panel .el-radio__input {
+  visibility: hidden;
+}
+</style>
