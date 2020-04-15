36035e7440b98a135f8c97a23c58f82f813d1d83
lucifer021 <1163302785@qq.com>
2020-04-16 00:47:33 +0800

    :bug: 修复重复点击路由报错问题

--- src/router/index.js
+++ src/router/index.js
@@ -131,4 +131,9 @@ const router = new VueRouter({
   // base: '/'
 })
 
+const originalPush = VueRouter.prototype.push
+VueRouter.prototype.push = function push(location) {
+  return originalPush.call(this, location).catch((err) => err)
+}
+
 export default router
