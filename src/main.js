921305c25c48b49c629fa9c7b1ab0ee94c89f30d
lyuhongzhi <lyuhongzhi@foxmail.com>
2020-04-30 20:10:26 +0800

    new branche

--- src/main.js
+++ src/main.js
@@ -4,14 +4,13 @@ import router from './router'
 import store from './store'
 import echarts from 'echarts'
 import ElementUI from 'element-ui'
-import VueLazyComponent from '@xunlei/vue-lazy-component'
 import 'element-ui/lib/theme-chalk/index.css'
 import '@babel/polyfill'
 
 Vue.prototype.$echarts = echarts
 Vue.config.productionTip = false
 Vue.use(ElementUI)
-Vue.use(VueLazyComponent)
+
 
 new Vue({
   router,
