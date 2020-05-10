1d200a781e9f515e54d3ad344b65e07552143e3c
lucifer021 <1163302785@qq.com>
2020-05-10 18:28:38 +0800

    更改生产环境 url

---
+++ new/js/chunk-59766489.1ce91f0b.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-59766489"],{"0117":function(n,t,e){},"7b52":function(n,t,e){"use strict";var i=e("0117"),c=e.n(i);c.a},f5d4:function(n,t,e){"use strict";e.r(t);var i=function(){var n=this,t=n.$createElement,e=n._self._c||t;return e("div",{staticClass:"view-container"},[e("div",{staticClass:"main-index"},[e("router-view")],1)])},c=[],a={mounted:function(){}},s=a,u=(e("7b52"),e("2877")),o=Object(u["a"])(s,i,c,!1,null,"44d0c8ae",null);t["default"]=o.exports}}]);
+//# sourceMappingURL=chunk-59766489.1ce91f0b.js.map
